a7c4cf127a4064d40c63bba9f17c9bb6d226f052
Koushik Dutta <koushd@gmail.com>
2011-02-27 13:25:58 -0800

    fixes

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,5 +4,10 @@ include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
+file := $(TARGET_RECOVERY_ROOT_OUT)/sbin/postrecoveryboot.sh
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/postrecoveryboot.sh | $(ACP)
+	$(transform-prebuilt-to-target)
+
 # include the non-open-source counterpart to this file
 -include vendor/samsung/fascinate/AndroidBoardVendor.mk
